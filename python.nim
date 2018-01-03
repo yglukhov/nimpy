3a8c2e0dfc0acfc36900fa66b3f5bda21043721f
Yuriy Glukhov <yuriy.glukhov@gmail.com>
2018-01-03 14:12:58 +0200

    Some additions

--- python.nim
+++ python.nim
@@ -22,10 +22,13 @@ type # Python3
         ob_next: pointer
         ob_prev: pointer
 
-    PyObjectObj = object
+    PyObjectObj {.pure, inheritable.} = object
         # extra: PyObject_HEAD_EXTRA # in runtime depends on traceRefs. see pyAlloc
         ob_refcnt: Py_ssize_t
         ob_type: pointer
+
+    PyObjectVarHeadObj = object of PyObjectObj
+        ob_size: Py_ssize_t
 
     PyModuleDef_Base = object
         ob_base: PyObjectObj
@@ -59,6 +62,142 @@ type # Python3
         smalltable: array[2, Py_ssize_t] # Don't refer! not available in python 3
         internal: pointer # Don't ever refer
 
+    Destructor = proc(o: PyObject) {.cdecl.}
+    Printfunc = proc(o: PyObject, f: File, c: cint): cint {.cdecl}
+    Getattrfunc = proc(o: PyObject, a: cstring): PyObject {.cdecl.}
+    Getattrofunc = proc(o, a: PyObject): PyObject {.cdecl.}
+    Setattrfunc = proc(o: PyObject, a: cstring, at: PyObject): cint {.cdecl.}
+    Setattrofunc = proc(o, a, at: PyObject): cint {.cdecl.}
+    Reprfunc = proc(o: PyObject): PyObject {.cdecl.}
+    Richcmpfunc = proc(a, b: PyObject, c: cint): PyObject {.cdecl.}
+    Getiterfunc = proc(a: PyObject): PyObject {.cdecl.}
+    Iternextfunc = proc(a: PyObject): PyObject {.cdecl.}
+    Descrgetfunc = proc(a, b, c: PyObject): PyObject {.cdecl.}
+    Descrsetfunc = proc(a, b, c: PyObject): cint {.cdecl.}
+    Initproc = proc(a, b, c: PyObject): cint {.cdecl.}
+    Newfunc = proc(typ: PyObject, a, b: PyObject): PyObject {.cdecl.}
+    Allocfunc = proc(typ: PyObject, sz: Py_ssize_t): PyObject {.cdecl.}
+
+    # 3
+    # typedef PyObject *(*getattrfunc)(PyObject *, char *);
+    # typedef PyObject *(*getattrofunc)(PyObject *, PyObject *);
+    # typedef int (*setattrfunc)(PyObject *, char *, PyObject *);
+    # typedef int (*setattrofunc)(PyObject *, PyObject *, PyObject *);
+    # typedef PyObject *(*reprfunc)(PyObject *);
+    # typedef Py_hash_t (*hashfunc)(PyObject *);
+    # typedef PyObject *(*richcmpfunc) (PyObject *, PyObject *, int);
+    # typedef PyObject *(*getiterfunc) (PyObject *);
+    # typedef PyObject *(*iternextfunc) (PyObject *);
+    # typedef PyObject *(*descrgetfunc) (PyObject *, PyObject *, PyObject *);
+    # typedef int (*descrsetfunc) (PyObject *, PyObject *, PyObject *);
+    # typedef int (*initproc)(PyObject *, PyObject *, PyObject *);
+    # typedef PyObject *(*newfunc)(struct _typeobject *, PyObject *, PyObject *);
+    # typedef PyObject *(*allocfunc)(struct _typeobject *, Py_ssize_t);
+
+    # 2
+    # typedef PyObject *(*getattrfunc)(PyObject *, char *);
+    # typedef PyObject *(*getattrofunc)(PyObject *, PyObject *);
+    # typedef int (*setattrfunc)(PyObject *, char *, PyObject *);
+    # typedef int (*setattrofunc)(PyObject *, PyObject *, PyObject *);
+    # typedef int (*cmpfunc)(PyObject *, PyObject *);
+    # typedef PyObject *(*reprfunc)(PyObject *);
+    # typedef long (*hashfunc)(PyObject *);
+    # typedef PyObject *(*richcmpfunc) (PyObject *, PyObject *, int);
+    # typedef PyObject *(*getiterfunc) (PyObject *);
+    # typedef PyObject *(*iternextfunc) (PyObject *);
+    # typedef PyObject *(*descrgetfunc) (PyObject *, PyObject *, PyObject *);
+    # typedef int (*descrsetfunc) (PyObject *, PyObject *, PyObject *);
+    # typedef int (*initproc)(PyObject *, PyObject *, PyObject *);
+    # typedef PyObject *(*newfunc)(struct _typeobject *, PyObject *, PyObject *);
+    # typedef PyObject *(*allocfunc)(struct _typeobject *, Py_ssize_t);
+
+    PyTypeObject3 = ptr object of PyObjectVarHeadObj
+        tp_name: cstring
+        tp_basicsize, tp_itemsize: Py_ssize_t
+
+        # Methods to implement standard operations
+        tp_dealloc: Destructor
+        tp_print: Printfunc
+
+        tp_getattr: Getattrfunc
+        tp_setattr: Setattrfunc
+
+        tp_as_async: pointer
+
+        tp_repr: Reprfunc
+
+        # Method suites for standard classes
+
+        tp_as_number: pointer
+        tp_as_sequence: pointer
+        tp_as_mapping: pointer
+
+
+        # More standard operations (here for binary compatibility)
+
+        tp_hash: pointer # hashfunc
+        tp_call: pointer # ternaryfunc
+        tp_str: Reprfunc
+        tp_getattro: Getattrofunc
+        tp_setattro: Setattrofunc
+
+        # Functions to access object as input/output buffer
+        tp_as_buffer: pointer
+
+        # Flags to define presence of optional/expanded features
+        tp_flags: culong
+
+        tp_doc: cstring
+
+        # call function for all accessible objects
+        tp_traverse: pointer
+
+        # delete references to contained objects
+        tp_clear: pointer # inquiry
+
+        # rich comparisons
+        tp_richcompare: Richcmpfunc
+
+        # weak reference enabler
+        tp_weaklistoffset: Py_ssize_t
+
+        # Iterators
+        tp_iter: Getiterfunc
+        tp_iternext: Iternextfunc
+
+        # Attribute descriptor and subclassing stuff
+        tp_methods: ptr PyMethodDef
+        tp_members: pointer # ptr PyMemberDef
+        tp_getset: pointer # ptr PyGetSetDef
+
+        tp_base: PyTypeObject3
+        tp_dict: PyObject
+
+        tp_descr_get: Descrgetfunc
+        tp_descr_set: Descrsetfunc
+        tp_dictoffset: Py_ssize_t
+
+        tp_init: Initproc
+        tp_alloc: Allocfunc
+        tp_new: Newfunc
+
+        tp_free: pointer # freefunc
+        tp_is_gc: pointer # inquiry /* For PyObject_IS_GC */
+
+        tp_bases: ptr PyObjectObj
+        tp_mro: PyObject # method resolution order. array?
+        tp_cache: PyObject # array?
+        tp_subclasses: ptr PyObjectObj
+        tp_weaklist: ptr PyObjectObj
+
+        tp_del: Destructor
+
+        # Type attribute cache version tag. Added in version 2.6 */
+        tp_version_tag: cuint
+        tp_finalize: Destructor
+
+    PyTypeObject = PyTypeObject3
+
 proc isNil*(p: PyObject): bool {.borrow.}
 
 var Py_BuildValue*: proc(f: cstring): PyObject {.cdecl, varargs.}
@@ -66,11 +205,23 @@ var PyTuple_Size*: proc(f: PyObject): Py_ssize_t {.cdecl.}
 var PyTuple_GetItem*: proc(f: PyObject, i: Py_ssize_t): PyObject {.cdecl.}
 var PyArg_ParseTuple*: proc(f: PyObject, fmt: cstring): cint {.cdecl, varargs.}
 var Py_None*: PyObject
+var PyType_Ready*: proc(f: PyTypeObject): cint {.cdecl.}
+
+# var PyList_Check*: proc(l: PyObject): cint {.cdecl.}
+var PyList_New*: proc(size: Py_ssize_t): PyObject {.cdecl.}
+var PyList_Size*: proc(l: PyObject): Py_ssize_t {.cdecl.}
+var PyList_GetItem*: proc(l: PyObject, index: Py_ssize_t): PyObject {.cdecl.}
+var PyList_SetItem*: proc(l: PyObject, index: Py_ssize_t, i: PyObject): cint {.cdecl.}
+
+var PyLong_AsLongLong*: proc(l: PyObject): int64 {.cdecl.}
+var PyFloat_AsDouble*: proc(l: PyObject): cdouble {.cdecl.}
+var PyBool_FromLong*: proc(v: clong): PyObject {.cdecl.}
 
 type PyModuleDesc = object
     name: cstring
     doc: cstring
     methods: seq[PyMethodDef]
+    types: seq[PyTypeObject]
 
 proc addMethod(m: var PyModuleDesc, name, doc: cstring, f: PyCFunction) =
     m.methods.add(PyMethodDef(ml_name: name, ml_meth: f, ml_flags: 1, ml_doc: doc))
@@ -116,14 +267,23 @@ proc initCommon(m: var PyModuleDesc) =
     load PyTuple_GetItem, "PyTuple_GetItem"
     load PyArg_ParseTuple, "_PyArg_ParseTuple_SizeT"
     load Py_None, "_Py_NoneStruct"
+    load PyType_Ready, "PyType_Ready"
+    # load PyList_Check, "PyList_Check"
+    load PyList_New, "PyList_New"
+    load PyList_Size, "PyList_Size"
+    load PyList_GetItem, "PyList_GetItem"
+    load PyList_SetItem, "PyList_SetItem"
+    load PyLong_AsLongLong, "PyLong_AsLongLong"
+    load PyFloat_AsDouble, "PyFloat_AsDouble"
+    load PyBool_FromLong, "PyBool_FromLong"
 
-    # PyTuple_Size = cast[type(PyTuple_Size)](gLibModule.symAddr("PyTuple_Size"))
-    # PyTuple_GetItem = cast[type(PyTuple_GetItem)](gLibModule.symAddr("PyTuple_GetItem"))
-    # PyArg_ParseTuple = cast[type(PyArg_ParseTuple)](gLibModule.symAddr("_PyArg_ParseTuple_SizeT"))
-    # Py_None =
     m.methods.add(PyMethodDef()) # Add sentinel
 
 proc initModuleTypes(p: PyObject, m: var PyModuleDesc) =
+    # let ty = cast[PyTypeObject3](pyAlloc(sizeof(PyTypeObject3)))
+    # ty.tp_name = "simple.TestType"
+    # ty.tp_basicsize = 4
+    # ty.tp_flags =
     discard
 
 proc initModule2(m: var PyModuleDesc) {.inline.} =
@@ -197,6 +357,10 @@ template staticJoin(s: varargs[string]): cstring =
 type PyValue[T] = object
     when T is string:
         buf: Py_buffer
+    elif T is seq|array:
+        val: PyObject
+    elif T is int8|bool:
+        val: int16
     else:
         val: T
 
@@ -209,7 +373,11 @@ proc toString*(b: Py_buffer): string =
 
 
 template pySignatureForType(t: typedesc[string]): string = "s*"
-template pySignatureForType(t: typedesc[PyObject]): string = "o"
+template pySignatureForType(t: typedesc[PyObject]): string = "O"
+
+template pySignatureForType(t: typedesc[bool]): string = "h"
+template pySignatureForType(t: typedesc[int8]): string = "h"
+template pySignatureForType(t: typedesc[int16]): string = "h"
 template pySignatureForType(t: typedesc[int32]): string = "i"
 template pySignatureForType(t: typedesc[int64]): string = "L"
 template pySignatureForType(t: typedesc[int]): string =
@@ -220,13 +388,52 @@ template pySignatureForType(t: typedesc[int]): string =
     else:
         {.error: "Unexpected int size".}
 
+template pySignatureForType(t: typedesc[uint8]): string = "B"
+template pySignatureForType(t: typedesc[uint16]): string = "H"
+template pySignatureForType(t: typedesc[uint32]): string = "I"
+template pySignatureForType(t: typedesc[uint64]): string = "K"
+template pySignatureForType(t: typedesc[uint]): string =
+    when sizeof(uint) == sizeof(uint32):
+        pySignatureForType(uint32)
+    elif sizeof(uint) == sizeof(uint64):
+        pySignatureForType(uint64)
+    else:
+        {.error: "Unexpected int size".}
+
+
 template pySignatureForType(t: typedesc[float32]): string = "f"
 template pySignatureForType(t: typedesc[float64]): string = "d"
+template pySignatureForType(t: typedesc[seq]): string = "O"
+template pySignatureForType(t: typedesc[array]): string = "O"
 
+proc pyObjToNim[T: int|int32|int64|int16|uint32|uint64|uint16|uint](o: PyObject, r: var T) {.inline.} =
+    r = T(PyLong_AsLongLong(o))
+
+proc pyObjToNim[T: float|float32|float64](o: PyObject, r: var T) {.inline.} =
+    r = T(PyFloat_AsDouble(o))
+
+proc pyObjToNim[T](o: PyObject, s: var seq[T]) =
+    # assert(PyList_Check(o) != 0)
+    let sz = int(PyList_Size(o))
+    assert(sz >= 0)
+    s = newSeq[T](sz)
+    for i in 0 ..< sz:
+        pyObjToNim(PyList_GetItem(o, i), s[i])
+
+proc pyObjToNim[T, I](o: PyObject, s: var array[I, T]) =
+    # assert(PyList_Check(o) != 0)
+    let sz = int(PyList_Size(o))
+    assert(sz == s.len)
+    for i in 0 ..< sz:
+        pyObjToNim(PyList_GetItem(o, i), s[i])
 
 proc pyValueToNim[T](v: PyValue[T]): T {.inline.} =
     when T is string:
         v.buf.toString()
+    elif T is seq|array:
+        pyObjToNim(v.val, result)
+    elif T is int8|bool:
+        T(v.val)
     else:
         v.val
 
@@ -247,6 +454,8 @@ iterator arguments(prc: NimNode): tuple[idx: int, name, typ, default: NimNode] =
             yield (iParam, pp[j], pp[^2], pp[^1])
             inc iParam
 
+proc nimArrToPy[T](s: openarray[T]): PyObject
+
 template nimValueToPy[T](v: T): PyObject =
     when T is void:
         v
@@ -265,10 +474,37 @@ template nimValueToPy[T](v: T): PyObject =
             Py_BuildValue("L", v)
         else:
             {.error: "Unkown int size".}
+    elif T is int8:
+        Py_BuildValue("b", v)
+    elif T is uint8:
+        Py_BuildValue("B", v)
+    elif T is int32:
+        Py_BuildValue("i", v)
+    elif T is uint32:
+        Py_BuildValue("I", v)
+    elif T is int16:
+        Py_BuildValue("h", v)
+    elif T is uint16:
+        Py_BuildValue("H", v)
+    elif T is int64:
+        Py_BuildValue("L", v)
+    elif T is uint64:
+        Py_BuildValue("K", v)
     elif T is float32 | float | float64:
         Py_BuildValue("d", float64(v))
+    elif T is seq|array:
+        nimArrToPy(v)
+    elif T is bool:
+        PyBool_FromLong(clong(v))
     else:
         {.error: "Unkown return type".}
+
+proc nimArrToPy[T](s: openarray[T]): PyObject =
+    let sz = s.len
+    result = PyList_New(sz)
+    for i in 0 ..< sz:
+        let o = nimValueToPy(s[i])
+        discard PyList_SetItem(result, i, o)
 
 proc makeWrapper(name, prc: NimNode): NimNode =
     let selfIdent = newIdentNode("self")
--- tests/simple.nim
+++ tests/simple.nim
@@ -1,4 +1,5 @@
 import ../python
+import algorithm
 
 proc greet(name: string): string {.exportpy.} =
     return "Hello, " & name & "!"
@@ -8,5 +9,9 @@ proc somethingThatReturnsNilString(): string {.exportpy.} =
 
 proc sumInts(a, b: int32): int {.exportpy.} = a + b
 proc sumFloats(a, b: float): float {.exportpy.} = a + b
-proc sumAssorted(a, b: int32, c: int, d: int64, e: float32, f: float64, g: float): float {.exportpy.} =
+proc sumAssorted(a, b: int32, c: uint8, d: int64, e: float32, f: float64, g: float): float {.exportpy.} =
     a.float + b.float + c.float + d.float + e + f + g
+
+proc reverseArray(a: seq[int]): seq[int] {.exportpy.} = a.reversed()
+proc reverseVec3(a: array[3, float]): array[3, float] {.exportpy.} = [a[2], a[1], a[0]]
+proc flipBool(b: bool): bool {.exportpy.} = not b
