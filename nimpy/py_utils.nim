78e6a9c45db3293cfd9c96f4de53d528819c6dc7
Yuriy Glukhov <yuriy.glukhov@gmail.com>
2021-10-21 11:41:22 +0300

    Fixed indentation

--- nimpy/py_utils.nim
+++ nimpy/py_utils.nim
@@ -4,52 +4,52 @@ import py_lib as lib
 import py_types
 
 proc incRef*(p: PPyObject) {.inline.} =
-    inc p.to(PyObjectObj).ob_refcnt
+  inc p.to(PyObjectObj).ob_refcnt
 
 proc decRef*(p: PPyObject) {.inline.} =
-    let o = p.to(PyObjectObj)
-    dec o.ob_refcnt
-    if o.ob_refcnt == 0:
-        pyLib.PyDealloc(p)
+  let o = p.to(PyObjectObj)
+  dec o.ob_refcnt
+  if o.ob_refcnt == 0:
+    pyLib.PyDealloc(p)
 
 proc checkObjSubclass*(o: PPyObject, flags: int): bool {.inline.} =
-    let typ = cast[PyTypeObject]((cast[ptr PyObjectObj](o)).ob_type)
-    (typ.tp_flags and flags.culong) != 0
+  let typ = cast[PyTypeObject]((cast[ptr PyObjectObj](o)).ob_type)
+  (typ.tp_flags and flags.culong) != 0
 
 proc checkObjSubclass*(o: PPyObject, ty: PyTypeObject): bool {.inline.} =
-    var typ = cast[PyTypeObject]((cast[ptr PyObjectObj](o)).ob_type)
-    (ty == typ) or pyLib.PyType_IsSubtype(typ, ty) != 0
+  var typ = cast[PyTypeObject]((cast[ptr PyObjectObj](o)).ob_type)
+  (ty == typ) or pyLib.PyType_IsSubtype(typ, ty) != 0
 
 proc conversionToStringError() =
   pyLib.PyErr_Clear()
   raise newException(ValueError, "Can't convert python obj to string")
 
 proc pyStringToNim*(o: PPyObject, output: var string): bool =
-    var s: ptr char
-    var l: Py_ssize_t
-    var b: PPyObject
+  var s: ptr char
+  var l: Py_ssize_t
+  var b: PPyObject
 
-    if checkObjSubclass(o, pyLib.PyUnicode_Type):
-        b = pyLib.PyUnicode_AsUTF8String(o)
-        if b.isNil:
-            conversionToStringError()
-        if pyLib.PyBytes_AsStringAndSize(b, addr s, addr l) != 0:
-            decRef b
-            conversionToStringError()
-    elif checkObjSubclass(o, pyLib.PyBytes_Type):
-        if pyLib.PyBytes_AsStringAndSize(o, addr s, addr l) != 0:
-            conversionToStringError()
-    else:
-        return
+  if checkObjSubclass(o, pyLib.PyUnicode_Type):
+    b = pyLib.PyUnicode_AsUTF8String(o)
+    if b.isNil:
+      conversionToStringError()
+    if pyLib.PyBytes_AsStringAndSize(b, addr s, addr l) != 0:
+      decRef b
+      conversionToStringError()
+  elif checkObjSubclass(o, pyLib.PyBytes_Type):
+    if pyLib.PyBytes_AsStringAndSize(o, addr s, addr l) != 0:
+      conversionToStringError()
+  else:
+    return
 
-    output = newString(l) # XXX: No init zero-init required here
-    if l != 0:
-        copyMem(addr output[0], s, l)
+  output = newString(l) # XXX: No init zero-init required here
+  if l != 0:
+    copyMem(addr output[0], s, l)
 
-    if not b.isNil:
-        decRef b
+  if not b.isNil:
+    decRef b
 
-    result = true
+  true
 
 macro generateRaiseCase(typ: PPyObject, typns, valns: string): untyped =
   ## generate the calls to `PyErr_GivenExceptionMatches` to check which exception
@@ -125,18 +125,18 @@ macro generateRaiseCase(typ: PPyObject, typns, valns: string): untyped =
   result.add caseStmt
 
 proc raisePythonError*() =
-    var typ, val, tb: PPyObject
-    pyLib.PyErr_Fetch(addr typ, addr val, addr tb)
-    pyLib.PyErr_NormalizeException(addr typ, addr val, addr tb)
+  var typ, val, tb: PPyObject
+  pyLib.PyErr_Fetch(addr typ, addr val, addr tb)
+  pyLib.PyErr_NormalizeException(addr typ, addr val, addr tb)
 
-    let vals = pyLib.PyObject_Str(val)
-    let typs = pyLib.PyObject_Str(typ)
-    var typns, valns: string
-    if unlikely(not (pyStringToNim(vals, valns) and pyStringToNim(typs, typns))):
-      raise newException(AssertionError, "Can not stringify exception")
-    decRef vals
-    decRef typs
+  let vals = pyLib.PyObject_Str(val)
+  let typs = pyLib.PyObject_Str(typ)
+  var typns, valns: string
+  if unlikely(not (pyStringToNim(vals, valns) and pyStringToNim(typs, typns))):
+    raise newException(AssertionError, "Can not stringify exception")
+  decRef vals
+  decRef typs
 
-    # generate the code for the checks to get the correct Python Exception and
-    # then generate the case statement to map to the equivalent Nim exception
-    generateRaiseCase(typ, typns, valns)
+  # generate the code for the checks to get the correct Python Exception and
+  # then generate the case statement to map to the equivalent Nim exception
+  generateRaiseCase(typ, typns, valns)
